65c2fa210297cea92c02b3ae1e44d7b7049b2bcb
MDRomzan <mdromjan483@gmail.com>
2021-11-08 13:14:22 +0600

    remove locale date server

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ async function run(){
         // get all appointments
         app.get("/appointments",verifyToken,async(req,res)=>{
             const email =req.query.email;
-            const date = new Date(req.query.date).toLocaleDateString();
+            const date = req.query.date;
             // console.log(date)
             const query ={email:email, date:date}
             const cursor =appointmentsCollection.find(query);
